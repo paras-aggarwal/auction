31890ee8c2e471db6ee9629aa67bac370c28e427
Paras Aggarwal <paras.aggarwal@outlook.com>
2023-12-23 17:01:39 +0100

    initial project skeleton

---
+++ biding/src/main/java/org/deutschebank/auction/biding/BidingApplication.java
@@ -0,0 +1,13 @@
+package org.deutschebank.auction.biding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidingApplication.class, args);
+	}
+
+}
---
+++ biding/src/test/java/org/deutschebank/auction/biding/BidingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deutschebank.auction.biding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
